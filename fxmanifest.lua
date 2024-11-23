6a56eeccdfa718e88026fa8ac371b5d849f8803a
Mr. Loe <157546293+MrLoescripts@users.noreply.github.com>
2024-11-23 05:04:14 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MrLoeScripts'
+description 'Loe-Noisecontrol - A resource for streamer-friendly serious RP that manages ambient noise and radio disruptions.'
+version '1.0.0'
+
+client_scripts {
+    'client.lua', 
+}
